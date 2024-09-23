4945ebce09d21d9323ad9a10a200f8f5654b829d
aurelien-f <aurelien.meltingk@gmail.com>
2024-09-23 14:53:52 +0200

    Etape 1 / 2 / 3

---
+++ README.md
@@ -0,0 +1,44 @@
+# Kata x Aurélien Feuillard
+
+## Etape 1
+
+* Installation de composer via brew
+  * `brew install composer`
+* Installation de symfony via composer
+  * `symfony new kata-symfony  --version="7.1.*" --webapp`
+* Installation de doctrine
+  * `composer require doctrine twig symfony/form`
+  * `composer require symfony/orm-pack`
+  * `composer require --dev symfony/maker-bundle`
+* Instancie le dépôt git local
+  * `git init`
+  * `git add .`
+  * `git commit -m "Initial commit"`
+* Créer une branche develop
+  * `git checkout -b develop`
+  
+## Etape 2
+
+* Création de la base de donnée
+  * `DATABASE_URL="sqlite:///%kernel.project_dir%/var/data.db" > .env.local`
+  * `php bin/console doctrine:database:create`
+
+* Création de l'entité Movie
+  * `php bin/console make:entity`
+  * `php bin/console make:migration`
+  * `php bin/console doctrine:migrations:migrate`
+
+* Migration de la base de donnée
+  * `php bin/console doctrine:migrations:migrate`
+
+## Etape 3
+
+* Création de l'entité Movie
+  * `php bin/console make:migration`
+  * `php bin/console doctrine:migrations:migrate`
+* Vérification de la table movie
+  * `sqlite3 var/data.db`
+  * `.tables`
+  * `.schema movie`
+  * `select * from movie;`
+
---
+++ src/Entity/Movie.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MovieRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MovieRepository::class)]
+class Movie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column]
+    private ?int $year = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $type = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $abstract = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getYear(): ?int
+    {
+        return $this->year;
+    }
+
+    public function setYear(int $year): static
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getAbstract(): ?string
+    {
+        return $this->abstract;
+    }
+
+    public function setAbstract(string $abstract): static
+    {
+        $this->abstract = $abstract;
+
+        return $this;
+    }
+}
