7310de5ac7ef142bf6b70b76d3863d033d66c697
aurelien-f <aurelien.meltingk@gmail.com>
2024-09-24 13:51:16 +0200

    Fix etape 10

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -10,7 +10,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Flasher\Prime\FlasherInterface;
 
 
 class MovieController extends AbstractController
